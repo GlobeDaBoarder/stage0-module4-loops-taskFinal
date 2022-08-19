e5c92211b33109ced29df64c3b9304ca52020fb6
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2022-08-19 15:46:46 +0300

    done #attempt2

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 0 ; i < cathetusLength; i++){
+            for (int j = cathetusLength; j > 0; j--){
+                if (cathetusLength - j  < cathetusLength - i - 1){
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.print(j);
+            }
 
+            for (int j = 2; j <= cathetusLength; j++){
+                if (j > i + 1){
+                    break;
+                }
+                System.out.print(j);
+            }
+
+            System.out.print("\n");
+        }
     }
 
     public static void main(String[] args) {
